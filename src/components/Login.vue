616fec340f6d9e215ace26c63d11e325f0db5a7f
Siriprayook <siriprayook@gmail.com>
2017-09-18 02:23:45 +1000

    added basic form for login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="login">
+
+    <h1 class="title">Login</h1>
+    <h2 class="subtitle"><a href="http://fontawesome.io/">Please enter your login details</a></h2>
+    <div class="tile is-ancestor">
+
+      <div class="tile is-parent is-3">
+        <article class="tile is-child box">
+          <h1 class="title">Login</h1>
+          <h2 class="subtitle"><a href="http://fontawesome.io/">Please enter your login details</a></h2>
+          <div class="block">
+            <p class="control has-icon">
+              <input class="input" type="email" placeholder="Email">
+              <span class="icon is-small">
+                <i class="fa fa-envelope"></i>
+              </span>
+            </p>
+            <p class="control has-icon">
+              <input class="input" type="password" placeholder="Password">
+              <span class="icon is-small">
+                <i class="fa fa-lock"></i>
+              </span>
+            </p>
+            <p class="control">
+              <button class="button is-success">
+                Login
+              </button>
+            </p>
+          </div>
+        </article>
+      </div>
+    </div>
+
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Add from '@/components/Add'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -10,6 +11,6 @@ export default new Router({
   routes: [
     { path: '/', name: 'home', component: Home },
     { path: '/add', name: 'add', component: Add },
-
+    { path: '/login', name: 'login', component: Login }
   ]
 })
