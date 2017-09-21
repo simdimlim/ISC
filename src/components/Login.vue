f974f79bed0a5243764f8d072c45b783989be9aa
Siriprayook <siriprayook@gmail.com>
2017-09-21 13:17:42 +1000

    draft login UI page - with api function call - basic form validation (checks empty fields)

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,34 +1,61 @@
 <template>
   <div class="login">
-
-    <h1 class="title">Login</h1>
-    <h2 class="subtitle"><a href="http://fontawesome.io/">Please enter your login details</a></h2>
-    <div class="tile is-ancestor">
-
-      <div class="tile is-parent is-3">
-        <article class="tile is-child box">
-          <h1 class="title">Login</h1>
-          <h2 class="subtitle"><a href="http://fontawesome.io/">Please enter your login details</a></h2>
-          <div class="block">
-            <p class="control has-icon">
-              <input class="input" type="email" placeholder="Email">
-              <span class="icon is-small">
-                <i class="fa fa-envelope"></i>
-              </span>
-            </p>
-            <p class="control has-icon">
-              <input class="input" type="password" placeholder="Password">
-              <span class="icon is-small">
-                <i class="fa fa-lock"></i>
-              </span>
-            </p>
-            <p class="control">
-              <button class="button is-success">
-                Login
-              </button>
-            </p>
+    <div class="login-wrapper columns">
+      <div class="column is-8 is-hidden-mobile hero-banner">
+        <section class="hero is-fullheight is-dark">
+          <div class="hero-body">
+            <div class="container section" style="background-color: transparent;">
+              <div class="has-text-right">
+                <h1 class="title is-1">Login</h1> <br>
+                <p class="title is-3">Please Enter Your Login Details</p>
+              </div>
+            </div>
           </div>
-        </article>
+        </section>  
+      </div>
+      <div class="column is-4">
+        <section class="hero is-fullheight">
+          <div class="hero-heading">
+            <div class="section has-text-centered">
+              <img src="../assets/icon1.png">
+              <p>ISC</p>
+            </div>
+          </div>
+          <div class="hero-body">
+            <div class="container">
+              <div class="columns">
+                <div class="column is-8 is-offset-2">
+                  <h1 class="avatar has-text-centered section">
+                    <img src="../assets/user.png">
+                  </h1>
+                  <div class="login-form">
+                    <p class="control has-icon has-icon-right">
+                      <input class="input email-input" type="text" placeholder="hello@world.org" v-model="account.email">
+                      <span class="icon user">
+                        <i class="fa fa-user"></i>
+                      </span>
+                    </p>
+                    <p class="control has-icon has-icon-right">
+                      <input class="input password-input" type="password" placeholder="●●●●●●●●●" v-model="account.password">
+                      <span class="icon user">
+                        <i class="fa fa-lock"></i>
+                      </span>
+                    </p>
+                    <p class="control login">
+                      <button class="button is-success is-outlined is-large is-fullwidth" :disabled="submitDisable" @click.prevent="login">Login</button>
+                    </p>
+                  </div>
+<!--                   <div class="section forgot-password">
+                    <p class="has-text-centered">
+                      <a href="#">Forgot password</a>
+                      <a href="#">Need help?</a>
+                    </p>
+                  </div> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>  
       </div>
     </div>
 
@@ -36,10 +63,33 @@
 </template>
 
 <script>
+import "bulma/bulma.sass"
+import axios from 'axios'
 export default {
   name: 'login',
   data () {
     return {
+      account: { email: '', password: '' }
+    }
+  },
+  methods: {
+    login () {
+      let url = 'http://localhost:3000/login'
+      axios.post(url, this.account).then(response => {
+        console.log('login success?')
+        this.$router.push('/')
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  },
+  computed: {
+    submitDisable () {
+      if (this.account.email && this.account.password) {
+        return false
+      } else {
+        return true
+      }
     }
   }
 }
