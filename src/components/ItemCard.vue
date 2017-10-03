444cd73610e50bc83cbc8054cc58c164eabf05a5
simdimlim <oldskooluv@hotmail.com>
2017-10-03 17:02:45 +1100

    designed draft for item cards at /card

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,53 @@
+<template>
+   <div class="card">
+   <div class="card-image">
+     <!-- <figure class="image is-4by3"> -->
+       <img src="../assets/boots.jpeg" alt="Placeholder image">
+     <!-- </figure> -->
+   </div>
+   <div class="card-content">
+     <div class="media">
+       <!-- <div class="media-left"> -->
+         <!-- <figure class="image is-48x48">
+           <img src="http://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+         </figure> -->
+       <!-- </div> -->
+       <div class="content">
+         <!-- <p class="subtitle is-4">Ivyrevel Flat Pointed Ankle Boot With Metal Trim</p>
+         <p>$98.83</p> -->
+         <p class="title is-4">Ivyrevel Flat Pointed Ankle Boot With Metal Trim</p>
+         <p class="subtitle is-5">$98.83</p>
+         <a>Visit site</a>
+         <br>
+         <p>Added on 11:09 PM - 1 Jan 2016</p>
+         <span class="icon is-small">
+           <i class="fa fa-lg fa-tag"></i>
+        </span> fashion
+       </div>
+     </div>
+
+     <!-- <div class="content">
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Phasellus nec iaculis mauris.
+       <br>
+       <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+     </div> -->
+   </div>
+ </div>
+</template>
+
+<script>
+import "bulma/bulma.sass"
+
+export default {
+  name: 'itemcard',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
