1f5bd9cb7257eff0f9724d5674ff8e6901bcb1ca
Siriprayook <siriprayook@gmail.com>
2017-10-04 14:37:42 +1100

    added filtering options - non functional

--- src/components/ui/Sidebar.vue
+++ src/components/ui/Sidebar.vue
@@ -1,18 +1,71 @@
 <template lang="html">
   <aside class="menu" style="text-align:left">
 
+    <p class="menu-label">Navigation</p>
+
+    <ul class="menu-list">
+      <div class="columns">
+        <div class="column is-9">
+          <div class="field search-bar-field">
+            <input class="input search-bar" placeholder="Search items...">
+          </div>
+        </div>
+        <div class="column is-3">
+          <a class="button add-item-btn"><i class="fa fa-search"></i></a>
+        </div>
+      </div>
+      <br>
+    </ul>
+
     <p class="menu-label">Filter</p>
 
     <ul class="menu-list">
-      <li><a style="padding-left:0px">Dashboard</a></li>
-      <li><a style="padding-left:0px">Profile</a></li>
+      <li><a style="padding-left:0px">Sort by Price</a></li>
+      <div class="columns">
+        <div class="column">
+          <input class="input" type="number" placeholder="Min $">
+        </div>
+        <div class="column">
+          <input class="input" type="number" placeholder="Max $">
+        </div>
+      </div>
+      <li>
+        <a style="padding-left:0px">Sort by Category</a>
+        <div class="select is-primary">
+          <select>
+            <option>None</option>
+            <option>Option 2</option>
+            <option>Rick</option>
+            <option>And</option>
+            <option>Morty</option>
+          </select>
+        </div>
+      </li>
+      <br>
+      <a class="checkbox" style="padding-left: 0px">
+        <input type="checkbox">
+         Favourites
+      </a>
+      <a class="button is-primary">Apply</a>
+      <br>
     </ul>
 
     <p class="menu-label">My Stores</p>
 
     <ul class="menu-list">
-      <li><a style="padding-left:0px">Dashboard</a></li>
-      <li><a style="padding-left:0px">Profile</a></li>
+      <li><a style="padding-left:0px">Stores</a></li>
+      <a class="checkbox" style="padding-left: 0px">
+        <input type="checkbox">
+         Amazon
+      </a>
+      <a class="checkbox" style="padding-left: 0px">
+        <input type="checkbox">
+         Ebay
+      </a>
+      <a class="checkbox" style="padding-left: 0px">
+        <input type="checkbox">
+         Coles
+      </a>      
     </ul>
 
   </aside>
@@ -30,5 +83,11 @@ export default {
 }
 .menu-list a:hover {
     color: #00d3d1;
+}
+.add-item-btn {
+  margin-right: 0px;
+  background-color: #00d3d1;
+  color: white;
+  border: none;
 }
 </style>
