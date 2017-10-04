c5ff95d33894c35e24d0354a0d7165b4030b9830
simdimlim <oldskooluv@hotmail.com>
2017-10-04 13:23:26 +1100

    added favourite button and changed category to button

--- src/components/ui/ItemCard.vue
+++ src/components/ui/ItemCard.vue
@@ -1,30 +1,36 @@
 <template>
    <div class="card">
-   <div class="card-image">
-       <img :src="img" alt="Placeholder image">
+      <div class="card-image">
+         <img :src="img" alt="Placeholder image">
+      </div>
+      <div class="card-content">
+         <a class="button is-pulled-right is-dark is-outlined" style="border:none;">
+            <span class="icon">
+               <i class="fa fa-trash"></i>
+            </span>
+         </a>
+         <a class="button is-pulled-right is-danger is-outlined" style="border:none;">
+            <span class="icon">
+               <i class="fa fa-heart"></i>
+            </span>
+         </a>
+         <div class="media">
+            <div class="content">
+               <p class="title is-4">{{title}}</p>
+               <p class="subtitle is-5">${{price}}</p>
+               <a>Visit site</a>
+               <br>
+               <p>Added {{timestamp}}</p>
+               <a class="button is-primary is-outlined">
+                <span class="icon is-small">
+                  <i class="fa fa-tag"></i>
+                </span>
+                <span>fashion</span>
+              </a>
+            </div>
+         </div>
+      </div>
    </div>
-   <div class="card-content">
-      <a class="button is-pulled-right is-dark is-outlined" style="border:none;">
-         <span class="icon">
-            <i class="fa fa-trash"></i>
-          </span>
-  </a>
-     <div class="media">
-       <div class="content">
-         <p class="title is-4">{{title}}</p>
-         <p class="subtitle is-5">${{price}}</p>
-         <a>Visit site</a>
-         <br>
-         <p>Added {{timestamp}}</p>
-         <font size="2">
-            <a><span class="icon is-small">
-           <i class="fa fa-lg fa-tag"></i>
-        </span> <span>fashion</span></a>
-     </font>
-       </div>
-     </div>
-   </div>
- </div>
 </template>
 
 <script>
